5c43aa95f15ac40d1810b0318b87387ecd407d36
Soniya <soniya19106208@gmail.com>
2024-08-19 20:54:24 +0530

    first commit

---
+++ attendance-tracking-backend/src/main/java/com/example/attendance_tracking_backend/AttendanceTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendance_tracking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceTrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceTrackingBackendApplication.class, args);
+	}
+
+}
